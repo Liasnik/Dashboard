2f4540d4e2427d46de5c0a49d015e2e20a834fc7
Liasnik <9426247@gmail.com>
2024-02-10 00:00:00 +0200

    added font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,10 @@
-import { Inter, Montserrat } from 'next/font/google';
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 export const montserrat = Montserrat({
   subsets: ['latin'],
   weight: ['400', '700'],
