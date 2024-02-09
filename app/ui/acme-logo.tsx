49fc1d8435b5156efb6446256c407decc5e7fdd6
Liasnik <9426247@gmail.com>
2024-02-09 23:42:13 +0200

    first commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,13 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { montserrat } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${montserrat.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg] md:h-16 md:w-16" />
+      <p className="text-[44px]">FinCo.</p>
     </div>
   );
 }
