6eb122db61585dc84e8dd4be724f9da2b7d6711d
Liasnik <9426247@gmail.com>
2024-02-15 19:34:27 +0200

    created dynamic route segment - edit page and function updateInvoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,9 +13,12 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+// Used Zod to update the expected types
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
+  // Извлечение данных из formData.
   //   const rawFormData = {
   //     customerId: formData.get('customerId'),
   //     amount: Number(formData.get('amount')),
@@ -30,9 +33,32 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
+  //Передача переменных в SQL-запрос
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  //очистки кэша клиента и выполнения нового запроса к серверу
+  revalidatePath('/dashboard/invoices');
+  //перенаправления пользователя на страницу счета
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  // Извлечение данных из formData.
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
   `;
 
   revalidatePath('/dashboard/invoices');
