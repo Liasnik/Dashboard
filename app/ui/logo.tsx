1a6aeb2b512df6249fe83ca48c5aaa3009652d05
Liasnik <9426247@gmail.com>
2024-02-18 14:50:22 +0200

    Added Authentication

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,12 +1,12 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { montserrat } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+export default function Logo() {
   return (
     <div
       className={`${montserrat.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg] md:h-16 md:w-16" />
+      <GlobeAltIcon className="h-12 w-12 shrink-0 rotate-[15deg] md:h-16 md:w-16" />
       <p className="text-[44px]">FinCo.</p>
     </div>
   );
